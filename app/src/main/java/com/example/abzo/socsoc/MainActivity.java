5fb90baf20c1287e707788fb323ea01b430b7561
TheAbzo <soon-ab@hotmail.com>
2018-07-27 01:38:54 +0200

    registerMain page V 1.0

--- app/src/main/java/com/example/abzo/socsoc/MainActivity.java
+++ app/src/main/java/com/example/abzo/socsoc/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.abzo.socsoc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    Button registerBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +18,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        registerBtn = (Button) findViewById(R.id.button_sign_up);//referncing
+        registerBtn.setOnClickListener(this);
 
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_sign_up: {
+
+                //intent
+                Intent intent = new Intent(MainActivity.this, RegisterMain.class);
+                startActivity(intent);
+                break;
+            }
+
+
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/abzo/socsoc/RegisterMain.java
@@ -0,0 +1,14 @@
+package com.example.abzo.socsoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_main);
+        getSupportActionBar().hide();
+    }
+}
