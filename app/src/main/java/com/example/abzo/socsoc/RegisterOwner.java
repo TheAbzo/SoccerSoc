4e2f716f085272c74b79d69af7ce27913b358a11
TheAbzo <soon-ab@hotmail.com>
2018-07-29 16:53:31 +0200

    register player finished(except back button)

---
+++ app/src/main/java/com/example/abzo/socsoc/RegisterOwner.java
@@ -0,0 +1,13 @@
+package com.example.abzo.socsoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterOwner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_owner);
+    }
+}
